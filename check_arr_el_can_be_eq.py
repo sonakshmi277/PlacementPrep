c4a13fc2de277121d0e778a23ad976f9238b3c90
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2025-03-16 20:11:59 +0530

    Check whether array elements can be made equal

---
+++ check_arr_el_can_be_eq.py
@@ -0,0 +1,22 @@
+#Function to check whether an array is equal or not
+def check(array,length):
+     for i in range(0, length):
+     
+          # Divide number by 2
+            while array[i] % 2 == 0:
+                array[i] //= 2
+
+          # Divide number by 3
+            while array[i] % 3 == 0:
+                array[i] //= 3
+
+     if array[i] != array[0]:
+         return False
+
+     return True
+
+#input array from user
+array = [50, 100, 75]
+
+#determine length of array and assign to length variable
+length=len(array)
