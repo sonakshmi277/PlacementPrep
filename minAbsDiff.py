ba5c0d97bd63d9cd3fe1b995380998f75225d63c
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2025-03-16 13:35:45 +0530

    New

--- minAbsDiff.py
+++ minAbsDiff.py
@@ -1,24 +1,30 @@
-#Write a program to find the sum of minimum absolute difference using python
-def sumOfMinAbsDifferences(arr,n):
-    # sort the given array
-    arr.sort()
-    
-    sum = 0
-         
-    sum += abs(arr[0] - arr[1]);
-         
-    sum += abs(arr[n - 1] - arr[n - 2]);
-         
-    
-    for i in range(1, n - 1):
-        sum += min(abs(arr[i] - arr[i - 1]),
-                  abs(arr[i] - arr[i + 1]))
-             
-    # required sum
-    return sum
-         
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
  
-# Driver code
-arr = [2, 5, 4, 3]
-n = len(arr)
-print( "Required Sum is", sumOfMinAbsDifferences(arr, n))
+
+Example 1:
+
+Input: arr = [4,2,1,3]
+Output: [[1,2],[2,3],[3,4]]
+Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        min_diff = sys.maxsize
+        arr.sort()
+        res = []
+        for i in range(len(arr)-1):
+
+            diff = arr[i+1] - arr[i]
+
+            if diff < min_diff:
+                min_diff = diff
+                res = [[arr[i],arr[i+1]]]
+            elif diff == min_diff:
+                res += [[arr[i],arr[i+1]]]
+        return res
