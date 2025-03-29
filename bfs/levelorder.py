e60405c802d49c9438d6f529d19bd4585f265095
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2025-03-29 11:08:35 +0530

    Level order Trav/BFS

---
+++ bfs/levelorder.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q=deque()
+        ans=[]
+        q.append(root)
+        while q:
+            qlen=len(q)
+            level=[]
+            for i in range(qlen):
+                node=q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            ans.append(level)
+                
+        return ans
+
+
+
+
+
+
+        
