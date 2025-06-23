15b05e3347a8d875608c9c7a43a101c42eb8dd04
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2025-06-23 16:21:21 +0530

    Day 4

--- numberOfSubsContainChar.py
+++ numberOfSubsContainChar.py
@@ -1,9 +1,14 @@
-s="abcgha"
-ct=0
-for i in range(len(s)):
-    st=""
-    for j in range(i,len(s)):
-                st+=s[j]
-                if("a" in st and "b" in st and "c" in st):
-                    ct+=1
-print(ct)
+def numberOfSubstrings(self, s: str) -> int:
+        last_seen = {'a': -1, 'b': -1, 'c': -1}
+        count = 0
+
+        for i in range(len(s)):
+            if s[i] in last_seen:
+                last_seen[s[i]] = i
+
+        # If all three seen, we can count substrings ending at i
+            if -1 not in last_seen.values():
+                min_index = min(last_seen.values())
+                count += min_index + 1  # substrings starting from 0 to min_index
+
+        return count
